21e8c03a0e904971d6111f2c22012dd31d0a6702
Gaurav Rana <em.gauravrana@gmail.com>
2024-09-08 01:56:22 +0530

    update the meta data

--- src/app/(pages)/blog/[...id]/page.tsx
+++ src/app/(pages)/blog/[...id]/page.tsx
@@ -8,10 +8,28 @@ export async function generateMetadata({
   params: { id: string };
 }): Promise<Metadata> {
   const post = await getPost(Number(params.id));
+  console.log(post);
   return {
     title: post.post.title,
     description: post.post.excerpt,
     openGraph: {
+      url: `https://technosysinc.com/blog/${params.id}`,
+      type: "article",
+      title: post.post.title,
+      description: post.post.excerpt,
+      images: [
+        {
+          url: post.post.featuredImage.node.mediaItemUrl,
+          width: 1200,
+          height: 630,
+          alt: post.post.title,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: post.post.title,
+      description: post.post.excerpt,
       images: [post.post.featuredImage.node.mediaItemUrl],
     },
   };
